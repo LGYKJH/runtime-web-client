01fd5699b18858cedb8f8153e1bccdd34d9803f6
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-11-15 12:09:24 +0900

    ✨ feat: implement login user api

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -5,59 +5,31 @@ export async function POST(request: Request) {
 
   try {
     const { email, password } = await request.json();
-
-    const apiUrl = `${BASE_URL}/user/login`;
-
     const body = { userId: email, userPw: password };
 
-    // 외부 API에 로그인 요청을 프록시하는 코드
-    const response = await fetch(apiUrl, {
+    const response = await fetch(BASE_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      credentials: "include",
       body: JSON.stringify(body),
     });
 
-    const contentType = response.headers.get("Content-Type");
-
-    let responseData;
-    if (contentType && contentType.includes("application/json")) {
-      responseData = await response.json();
-    } else {
-      responseData = await response.text();
-    }
-
-    if (!responseData.result) {
-      const errorMessage =
-        typeof responseData === "object"
-          ? JSON.stringify(responseData)
-          : responseData;
-
+    if (!response.ok) {
+      const errorData = await response.text();
       return NextResponse.json(
-        { error: "Error: " + errorMessage },
+        { error: errorData },
         { status: response.status }
       );
     }
 
-    // 외부 API 응답을 그대로 반환
-    return NextResponse.json(
-      {
-        message: "로그인이 성공했습니다.",
-        data: responseData,
-      },
-      { status: 200 }
-    );
+    const responseData = await response.json();
+    return NextResponse.json({ message: "로그인 성공", data: responseData });
   } catch (error) {
-    // error가 Error 타입인지 확인하여 안전하게 message를 접근
     const errorMessage =
       error instanceof Error
         ? error.message
         : "알 수 없는 오류가 발생했습니다.";
-    return NextResponse.json(
-      { error: "로그인 중 오류가 발생했습니다 : " + errorMessage },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
