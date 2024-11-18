292bbcba5ef946ae2b49fba7f7af3591229e8c3f
kimnerd625 <jihunkim625@yonsei.ac.kr>
2024-11-18 17:51:46 +0900

    💄 style: LeftBar.tsx 컴포넌트 UI 구현중

--- src/app/dashboard/_components/LeftBar.tsx
+++ src/app/dashboard/_components/LeftBar.tsx
@@ -1,21 +1,26 @@
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-} from "@/components/ui/sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Label } from "@/components/ui/label";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarHeader } from "@/components/ui/sidebar";
 import React from "react";
 
 const LeftBar = () => {
   return (
     <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+      <SidebarHeader className="pt-[100px] px-8 flex flex-col justify-start items-start gap-">
+        <div className="w-full flex flex-row justify-start items-center gap-x-2">
+          <Avatar className="w-6 h-6">
+            <AvatarImage src="https://github.com/shadcn.png" className="w-6 h-6" />
+            <AvatarFallback>아바타</AvatarFallback>
+          </Avatar>
+          <span className="font-normal text-sm">이름</span>
+        </div>
+      </SidebarHeader>
+      <SidebarContent className="pt-6 px-8">
+        <Label className="font-normal py-1.5 text-secondary">관리 탭</Label>
+        <Label className="font-normal py-1.5 text-secondary">주간 계획</Label>
+        <Label className="font-normal py-1.5 text-secondary">목표</Label>
+        <Label className="font-normal py-1.5 text-secondary">월간 캘린더</Label>
       </SidebarContent>
-      <SidebarFooter />
     </Sidebar>
   );
 };
