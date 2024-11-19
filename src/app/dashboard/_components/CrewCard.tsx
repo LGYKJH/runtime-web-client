b9f087e352277ff0f97648bb3eb2e0c624df9860
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-11-19 13:01:14 +0900

    ✨ feat: CrewCard.tsx 컴포넌트 UI 구현

---
+++ src/app/dashboard/_components/CrewCard.tsx
@@ -0,0 +1,47 @@
+import { Badge } from "@/components/ui/badge";
+import React from "react";
+
+interface CrewCardProps {
+  title: string;
+  imageURL: string;
+}
+
+const CrewCard = () => {
+  return (
+    <div className="w-full flex flex-row justify-start items-center gap-x-4 cursor-pointer">
+      <div className="overflow-hidden rounded-lg">
+        <img
+          alt="이미지"
+          className="w-[240px] h-[128px] object-cover"
+          src="https://placehold.co/240x128"
+        />
+      </div>
+      <div className="bg-muted h-full flex-1 flex flex-col justify-start items-start px-6 py-6 rounded-lg">
+        <div className="w-full flex flex-row justify-between items-center">
+          <h5 className="text-base font-bold text-primary">
+            한강 크루팀 ( 신입 모집 )
+          </h5>
+          <Badge className="w-16 h-6 flex flex-row justify-center items-center">
+            대기 중
+          </Badge>
+        </div>
+        <span className="pt-2 text-sm text-secondary">
+          퇴근 후 부담없이 즐기는 루틴 런닝! **초보러너 환영
+        </span>
+        <div className="w-full flex flex-row justify-start items-center mt-4 gap-x-6">
+          <span className="text-xs text-secondary font-normal">
+            &#35; 마라톤
+          </span>
+          <span className="text-xs text-secondary font-normal">
+            &#35; 한강대로변
+          </span>
+          <span className="text-xs text-secondary font-normal">
+            &#35; 매주 금요일 오후 5시부터 7시
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CrewCard;
--- src/app/dashboard/_components/CrewList.tsx
+++ src/app/dashboard/_components/CrewList.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import CrewListMenu from "./CrewListMenu";
+import CrewCard from "./CrewCard";
 
 const CrewList = () => {
   const [menuType, setMenuType] = useState<string>("크루");
@@ -9,6 +10,9 @@ const CrewList = () => {
   return (
     <div className="w-full flex flex-col justify-start items-center gap-y-7 px-10 py-7">
       <CrewListMenu menuType={menuType} setMenuType={setMenuType} />
+      <CrewCard />
+      <CrewCard />
+      <CrewCard />
     </div>
   );
 };
