461612444e3f1355b02033488ecda5fe34c8498c
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-11-19 17:52:44 +0900

    🐛 fix: 동적 라우팅 비동기 처리 에러 수정

--- src/app/dashboard/crew/[crewId]/page.tsx
+++ src/app/dashboard/crew/[crewId]/page.tsx
@@ -2,8 +2,12 @@ import { useRouter } from "next/navigation";
 import CrewSection from "../_sections/CrewSection";
 import RightBar from "../../_components/RightBar";
 
-export default function CrewPage({ params }: { params: { crewId: string } }) {
-  const { crewId } = params;
+export default async function CrewPage({
+  params,
+}: {
+  params: { crewId: string };
+}) {
+  const { crewId } = await params; // 비동기 처리
   return (
     <main className="w-full flex flex-row h-screen overflow-hidden">
       <CrewSection crewId={crewId} />
