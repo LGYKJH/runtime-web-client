d2cc8552ca56e196c78114c785a44ea811c9f537
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-11-25 20:27:16 +0900

    🐛 fix: middleware.ts 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,11 @@ export async function middleware(request: NextRequest) {
 
   // Access Token 또는 Refresh Token이 없으면 로그인 페이지로 리다이렉트
   if (!accessToken && !refreshToken) {
-    return NextResponse.redirect(`${process.env.BASE_URL}/users/login`);
+    return NextResponse.redirect(`http://localhost:3000/users/login`);
+  }
+
+  if (!refreshToken) {
+    return NextResponse.redirect(`http://localhost:3000/users/login`);
   }
 
   // Validate Token 한 번만 호출
@@ -20,7 +24,7 @@ export async function middleware(request: NextRequest) {
 
   if (!validationResponse.isValid) {
     // 토큰이 유효하지 않으면 로그인 페이지로 리다이렉트
-    return NextResponse.redirect(`${process.env.BASE_URL}/users/login`);
+    return NextResponse.redirect(`http://localhost:3000/users/login`);
   }
 
   // 요청을 그대로 전달
